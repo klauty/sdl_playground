ffd3248e2eec408c72160fa4201a59764bb087e8
klauty <kaue.developer@gmail.com>
2024-08-20 20:42:47 -0300

    atualizando arquivo de build para linux

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/zsh
 
-#MacOs
-clang hello_sdl3.c -I/usr/local/include/ -o hello_sdl3 -L/usr/local/lib/ -lSDL3 -rpath /usr/local/lib/ 
+if [[ $1 == "mac" ]] then
+	echo "Mac"
+	clang hello_sdl3.c -I/usr/local/include/ -o hello_sdl3 -L/usr/local/lib/ -lSDL3 -rpath /usr/local/lib/ 
+fi
+
+if [[ $1 == "linux" ]] then
+	echo "Linux"
+	clang hello_sdl3.c  -o hello_sdl3-v2  -I/usr/include/ -L/usr/lib/ -lSDL3 
+fi
+
